99cdbe210011379204806156a8e37ea855e5cf30
Shiva pandey <107467698+Shivastoic@users.noreply.github.com>
2025-09-05 14:16:58 +0530

    Wrap main pages with ReactLenis for smooth scrolling
    Added the ReactLenis root wrapper to the About, Contact, and Home pages to enable smooth scrolling effects using the lenis library. This change prepares the app for enhanced scroll-based UI interactions.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,19 +1,22 @@
 import AboutFounders from "@/app/about/about-founders";
 import Container from "@/app/components/container";
 import AboutIntro from "@/app/about/intro";
+import { ReactLenis } from 'lenis/react';
 
 export default function About() {
 
     return (
 
-        <main className="">
-            <Container>
-                <div className="flex flex-col gap-20 pt-12 pb-16 md:pt-26 md:pb-20">
-                    <AboutIntro />
-                    <AboutFounders />
-                </div>
-            </Container>
-        </main>
+        <ReactLenis root>
+            <main className="">
+                <Container>
+                    <div className="flex flex-col gap-20 pt-12 pb-16 md:pt-26 md:pb-20">
+                        <AboutIntro />
+                        <AboutFounders />
+                    </div>
+                </Container>
+            </main>
+        </ReactLenis>
 
     )
 
